b55379c318ed375a29b753cad81128ac2ff4921b
anonim20dd <146993399+anonim20dd@users.noreply.github.com>
2025-11-15 12:01:17 +0300

    Update logo image source in logo.lua

--- logo.lua
+++ logo.lua
@@ -22,7 +22,7 @@ ui.setAsynchronousImagesLoading(true)
 --Image Config table that is used by the script. 
 --Duplicate the entire table and increment the numbers by 1.
 --Follow the example from image_0 and image_1 to get an understanding of how it works.
-local image_0_source = 'https://raw.githubusercontent.com/anonim20dd/script/main/cml_gaming_beyaz_kirmizi_transparent.gif'
+local image_0_source = 'https://raw.githubusercontent.com/anonim20dd/script/main/CmlYeniLogoo_heartbeat.gif'
 local image_1_source = ''
 
 -------------------------------------------------------------------
